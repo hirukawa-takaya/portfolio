24fa4dab047147a9c1adaa4a83227b896e37291b
”hirukawa” <hiru0621@gmail.com>
2020-06-16 07:31:19 +0000

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    log_out
+    redirect_to root_path
   end
   
   private
@@ -21,6 +23,9 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
   end
   
-  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
   
 end
