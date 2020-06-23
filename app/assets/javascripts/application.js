950bac5317311258428f8c816fce374318dfa55c
”hirukawa” <hiru0621@gmail.com>
2020-06-23 07:38:17 +0000

    add ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
-//= require rails-ujs
 //= require jquery
+//= require rails-ujs
 //= require bootstrap
-//= require turbolinks
 //= require_tree .
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,21 +5,17 @@ def index
 end
     
 def create
+    @topic = Topic.find(params[:topic_id])
     like = Like.new
     like.user_id = current_user.id
     like.topic_id = params[:topic_id]
-
-    if like.save
-      redirect_to topics_path, success: 'お気に入りに登録しました'
-    else
-      redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
-    end
+    like.save
 end
 
 def destroy
+    @topic = Topic.find(params[:topic_id])
     @like = Like.find_by(topic_id: params[:topic_id], user_id: current_user.id)
     @like.destroy
-    redirect_to topics_path, success: 'お気に入りを解除しました'
 end
 
 
