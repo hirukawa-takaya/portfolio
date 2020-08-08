7ce55b6aa43c7b80169220cfb3c184a132471fb8
”hirukawa” <hiru0621@gmail.com>
2020-08-08 03:49:18 +0000

    fix design

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_topic_path
-    assert_response :success
-  end
-
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,22 +2,4 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
-  test "invalid signup information" do
-    get new_user_path
-    assert_no_difference 'User.count' do
-      post users_path, params:{user:{name: "", email: "hoge@example",
-                                    password: "foo", password_confirmation: "bar"}}
-    end
-    assert_template 'users/new'
-  end
-  
-  test "valid signup information" do
-    get new_user_path
-    assert_difference 'User.count', 1 do
-      post users_path, params:{user:{name: "hoge",email: "hoge@example.com",
-                                    password: "foobar", password_confirmation: "foobar"}}
-    end
-    #assert_template ''
-  end
-  
 end
